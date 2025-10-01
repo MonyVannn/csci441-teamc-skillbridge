6d404fb5110140d55f292a5fb24519fd2fef3028
Monyvann <m.men6603@gmail.com>
2025-09-30 23:37:54 -0500

    Feature: display count of unresponded application

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
 import { getUser, isAuthenticated } from "@/lib/actions/user";
 import HeaderContent from "./HeaderContent";
+import { getTotalUnrespondedApplication } from "@/lib/actions/application";
 
 const Header = async () => {
   const authenticated = await isAuthenticated();
   const user = authenticated ? await getUser() : null;
-  return <HeaderContent user={user} />;
+  const applications = await getTotalUnrespondedApplication();
+  return (
+    <HeaderContent user={user} totalUnrespondedApplications={applications} />
+  );
 };
 
 export default Header;
